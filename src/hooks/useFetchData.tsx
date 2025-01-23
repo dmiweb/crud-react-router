0490e99c7cc03c11e46e97f2f016eda9aee46bae
dmiweb <dimbons@mail.ru>
2025-01-23 10:59:09 +0300

    fix files

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,85 +1,67 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import { TPost } from '../models/index';
 
-
-// export type TPost = {
-//     id: number;
-//     title: string;
-//     content: string;
-//     created?: number; 
-//     [key: string]: any
-//   };
-
 type FetchResult = {
   data: TPost | TPost[] | null,
   loading: boolean,
   error: string | null,
 };
   
-  export const useFetchData = <T extends TPost>(
-      url: string | null,
-      opts: RequestInit = {},
-    ): [FetchResult, () => void] => {
-      const [data, setData] = useState<T | T[] | null>(null);
-      const [loading, setLoading] = useState<boolean>(false);
-      const [error, setError] = useState<string | null>(null);
-      const lastAddedCreated = useRef<number | null>(null); // Используем useRef для created
-      const isFetching = useRef<boolean>(false);
-      const initialFetchCompleted = useRef<boolean>(false);
-    
-      const fetchNews = useCallback(async () => {
-        if (!url || isFetching.current) return;
-    
-        isFetching.current = true;
-        setLoading(true);
-        setError(null);
-    
-        try {
-          const response = await fetch(url, { ...opts });
-    
-          if (!response.ok) {
-            throw new Error(`Ошибка! Status: ${response.status}`);
-          }
-             const responseData = response.status !== 204 ? await response.json() : null;
-    
-          if (responseData) {
-            // Убеждаемся, что данные это массив, если это так, то сортируем и ищем максимальный created
-            if (Array.isArray(responseData)) {
-              responseData.sort((a, b) => (b.created || 0) - (a.created || 0));
-    
-              if (
-                initialFetchCompleted.current === false ||
-                (responseData[0]?.created || 0) > (lastAddedCreated.current || 0)
-              ) {
+export const useFetchData = (
+  url: string | null,
+  opts: RequestInit = {},
+): [FetchResult] => {
+  const [data, setData] = useState<TPost | TPost[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const lastAddedCreated = useRef<number | null>(null);
+  const isFetching = useRef<boolean>(false);
+
+  const fetchNews = useCallback(async () => {
+      if (!url || isFetching.current) return;
+
+    isFetching.current = true;
+    setLoading(true);
+    setError(null);
+
+    try {
+        const response = await fetch(url, { ...opts });
+
+      if (!response.ok) {
+        throw new Error(`Ошибка! Status: ${response.status}`);
+        }
+         const responseData = response.status !== 204 ? await response.json() : null;
+
+      if (responseData) {
+          // Убеждаемся, что данные это массив, если это так, то сортируем и ищем максимальный created
+        if (Array.isArray(responseData)) {
+          responseData.sort((a, b) => (b.created || 0) - (a.created || 0));
+
+          if ( lastAddedCreated.current === null || (responseData[0]?.created || 0) > lastAddedCreated.current) {
                 setData(responseData);
-                lastAddedCreated.current = responseData[0]?.created || 0;
-                initialFetchCompleted.current = true;
+              lastAddedCreated.current = responseData[0]?.created || 0;
               }
             } else {
-              //Если это не массив, то просто сетим данные
-                setData(responseData);
-                lastAddedCreated.current = responseData?.created;
-                initialFetchCompleted.current = true;
+          //Если это не массив, то просто сетим данные
+            setData(responseData);
+            lastAddedCreated.current = responseData?.created;
             }
-          }
-        } catch (err) {
-          setError(`${err}`);
-        } finally {
-          setLoading(false);
+         }
+      } catch (err) {
+         setError(`${err}`);
+     } finally {
+         setLoading(false);
           isFetching.current = false;
-        }
-      }, [url, opts]);
-    
-      useEffect(() => {
-        fetchNews();
-      }, [fetchNews]);
-    
-      const forceUpdate = useCallback(() => {
-        fetchNews();
-      }, [fetchNews]);
-    
-      return [{ data, loading, error }, forceUpdate];
-    };
+       }
+  }, [url, opts]);
+
+useEffect(() => {
+      fetchNews();
+  }, [fetchNews]);
+
+
+return [{ data, loading, error }];
+};
 
 
 // import { useState, useEffect } from "react";
